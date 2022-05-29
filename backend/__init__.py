0b242030afd219c2e2fa3508f5a266620a0b1fff
ikn1062 <ishaannarain2022@u.northwestern.edu>
2022-05-28 22:27:20 -0500

    Added more to init

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,3 +1,4 @@
 from .request_payments import request_payment
 from .login import login
-from .library_code import update_next, get_requests, connect, Requests, Users, get_venmo_id, get_access_token
+from .library_code import update_next, get_requests, connect, Requests, Users, get_venmo_id, get_access_token, \
+    get_phone_number
